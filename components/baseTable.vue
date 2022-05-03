b6c39bb8795096543ef63988017390db2fe66a0c
Shivam Gupta <66762938+Shivam755@users.noreply.github.com>
2022-05-03 19:51:49 +0530

    first commit

---
+++ components/baseTable.vue
@@ -0,0 +1,42 @@
+<template>
+    <table class="rounded text-2xl">
+        <tr class="rounded bg-emerald-900 text-emerald-200" >
+            <th class="border-2 border-emerald-900 px-3 py-2 rounded" v-for="header in TableData.headings" :key="header">{{header}}</th>
+            <th class="border-2 border-emerald-900 px-3 py-2 rounded"> </th>
+        </tr>
+        <tr class="bg-emerald-200 text-emerald-900" v-for="row in TableData.data" :key="row[0]">
+            <td class="border-2 border-emerald-900 px-3 py-2 rounded" v-for="dat in row" :key="dat">{{dat}}</td>
+            <td class="border-2 border-emerald-900 px-3 py-2 rounded">
+                <button @click.prevent="showDetails(row)" class="m-2 rounded outline-0 text-3xl bg-lime-600 text-emerald-200 p-3">
+                    Show data
+                </button>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <slot></slot>
+            </td>
+        </tr>
+    </table>
+    
+</template>
+
+<script lang="ts">
+export default {
+    name:"BaseTable",
+    props:{
+        TableData:{
+            type:Object,
+            default: {
+                headings:["None"],
+                data:[['No data to display']]
+            }
+        }
+    },
+    methods:{
+        showDetails(row){
+            this.$emit('show-details', row)
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="flex flex-col h-screen w-screen justify-center items-center">
+
+        <BaseTable @show-details="show" :TableData="tableData">
+            <button @click="clear" class="m-2 rounded outline-0 text-3xl bg-lime-600 text-emerald-200 p-3">
+                Clear
+            </button>
+        </BaseTable>
+
+        <div class="text-3xl text-emerald-900" v-if="displayData != ''">
+            <h1>Details:</h1>
+            <p><b>Roll no:</b> {{displayData[0]}} </p>
+            <p><b>Name:</b> {{displayData[1]}} </p>
+            <p><b>Std:</b> {{displayData[2]}} </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import BaseTable from "@/components/baseTable.vue"
+export default {
+    name:'Index',
+    components:{
+        BaseTable
+    },
+    data(){
+        return{
+            tableData:{
+                headings:["Roll no", "Name", "Std"],
+                data:[
+                    ["4503", "Rhea Menon", "SYBsc"],
+                    ["4517", "Shivam Gupta", "SYBSc"],
+                    ["4516", "Harshal Gondkar", "SYBSc"],
+                    ["4515", "Jyoti Ghosh", "SYBSc"],
+                    ["4514", "Tejas Pophale", "SYBSc"],
+                ]
+            },
+            displayData:''
+        }
+    },
+    methods:{
+        show(e){
+            this.displayData=e
+            console.log(this.displayData)
+        },
+        clear(){
+            this.displayData=''
+        }
+    }
+}
+</script>
